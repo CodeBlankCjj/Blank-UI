f48b5c1ee28fa4155df510455ff3fcffe12298d4
CodeBlankCjj <cj13480394274@163.com>
2021-06-07 13:03:35 +0800

    添加 click

--- src/Button.vue
+++ src/Button.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name="loading"></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div  class="content">
       <slot></slot>
     </div>
@@ -18,6 +18,10 @@ export default {
       validator(value) {
         return value === 'left' || value === 'right';
       }
+    },
+    loading:{
+      type: Boolean,
+      default: false
     }
   }
 }
