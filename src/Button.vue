64fc286e42e5135986017ba533b915211ce22f7d
CodeBlankCjj <cj13480394274@163.com>
2021-06-07 13:35:00 +0800

    添加 Button-group.vue

--- src/Button.vue
+++ src/Button.vue
@@ -26,7 +26,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @keyframes spin {
   0%{ transform: rotate(0deg)}
   100%{ transform: rotate(360deg)}
@@ -38,7 +38,7 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  display: flex;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: middle;
